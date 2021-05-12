03881b95bc68fb9d6d853543c1ae1b14d2259f36
Augusto Renan <augustorenanss@gmail.com>
2021-05-11 21:22:16 -0300

    website v1

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const aboutMe = document.getElementById("about-me");
+const linkedIn = document.getElementById("linked-in");
+const option = document.getElementById("option");
+const optionsList = document.getElementById("options-list");
+
+aboutMe.addEventListener("click", () => {
+  const box = new WinBox("/about-me", {
+    x: "center",
+    y: "center",
+    html: "<h1>Apaixonado por Node.js</h1>",
+  });
+});
+
+optionsList.addEventListener("mouseover", (event) => {
+  renderOption(event.target.innerText);
+});
+
+optionsList.addEventListener("mouseout", (event) => {
+  option.textContent = "";
+});
+
+function renderOption(text) {
+  option.textContent = " cd " + text;
+}
